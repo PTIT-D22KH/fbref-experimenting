0402a4da1aeb583849d21cc94092af188fb6b1a2
duongvu5 <vucongtuanduong5@gmail.com>
2024-08-29 04:25:10 +0000

    add files

--- fbref-players/epl/match_comparision.py
+++ fbref-players/epl/match_comparision.py
@@ -1,4 +1,3 @@
-
 import fbrefdata as fb
 import statsbombpy as st
 import mplsoccer as mp
@@ -9,6 +8,65 @@ import streamlit as st
 import json
 import os
 import shutil
+
+def create_radar_chart(params, low, high, lower_is_better, player1_values, player2_values, player1_name, player2_name, team1_name, team2_name, match1_name, match2_name):
+    """Create radar chart comparing two players."""
+    radar = mp.Radar(params, low, high, lower_is_better=lower_is_better, round_int=[False]*len(params), num_rings=4, ring_width=1, center_circle_radius=1)
+    
+    URL1 = ('https://raw.githubusercontent.com/googlefonts/SourceSerifProGFVersion/main/fonts/SourceSerifPro-Regular.ttf')
+    serif_regular = mp.FontManager(URL1)
+    URL2 = ('https://raw.githubusercontent.com/googlefonts/SourceSerifProGFVersion/main/fonts/SourceSerifPro-ExtraLight.ttf')
+    serif_extra_light = mp.FontManager(URL2)
+    URL3 = ('https://raw.githubusercontent.com/google/fonts/main/ofl/rubikmonoone/RubikMonoOne-Regular.ttf')
+    rubik_regular = mp.FontManager(URL3)
+    URL4 = 'https://raw.githubusercontent.com/googlefonts/roboto/main/src/hinted/Roboto-Thin.ttf'
+    robotto_thin = mp.FontManager(URL4)
+    URL5 = ('https://raw.githubusercontent.com/google/fonts/main/apache/robotoslab/RobotoSlab%5Bwght%5D.ttf')
+    robotto_bold = mp.FontManager(URL5)
+    
+    fig, axs = mp.grid(figheight=14, grid_height=0.915, title_height=0.06, endnote_height=0.025, title_space=0, endnote_space=0, grid_key='radar', axis=False)
+    
+    radar.setup_axis(ax=axs['radar'])
+    rings_inner = radar.draw_circles(ax=axs['radar'], facecolor='#ffb2b2', edgecolor='#fc5f5f')
+    radar_output = radar.draw_radar_compare(player1_values, player2_values, ax=axs['radar'], kwargs_radar={'facecolor': '#00f2c1', 'alpha': 0.6}, kwargs_compare={'facecolor': '#d80499', 'alpha': 0.6})
+    radar_poly, radar_poly2, vertices1, vertices2 = radar_output
+    range_labels = radar.draw_range_labels(ax=axs['radar'], fontsize=25, fontproperties=robotto_thin.prop)
+    param_labels = radar.draw_param_labels(ax=axs['radar'], fontsize=25, fontproperties=robotto_thin.prop)
+    axs['radar'].scatter(vertices1[:, 0], vertices1[:, 1], c='#00f2c1', edgecolors='#6d6c6d', marker='o', s=150, zorder=2)
+    axs['radar'].scatter(vertices2[:, 0], vertices2[:, 1], c='#d80499', edgecolors='#6d6c6d', marker='o', s=150, zorder=2)
+    
+    endnote_text = axs['endnote'].text(0.99, 0.5, 'Inspired By: StatsBomb / Rami Moghadam', fontsize=15, fontproperties=robotto_thin.prop, ha='right', va='center')
+    title1_text = axs['title'].text(0.01, 0.65, player1_name, fontsize=25, color='#01c49d', fontproperties=robotto_bold.prop, ha='left', va='center')
+    title2_text = axs['title'].text(0.01, 0.25, team1_name, fontsize=20, fontproperties=robotto_thin.prop, ha='left', va='center', color='#01c49d')
+    title3_text = axs['title'].text(0.99, 0.65, player2_name, fontsize=25, fontproperties=robotto_bold.prop, ha='right', va='center', color='#d80499')
+    title4_text = axs['title'].text(0.99, 0.25, team2_name, fontsize=20, fontproperties=robotto_thin.prop, ha='right', va='center', color='#d80499')
+    
+    fig.set_facecolor('#f2dad2')
+    st.pyplot(fig)
+
+def compare_players_and_create_radar(merged_df, player1, player2, match1_name, match2_name, selected_params, param_mapping, lower_is_better):
+    """Compare two players and create a radar chart."""
+    col = [param_mapping[param] for param in selected_params]
+    player1_values = get_player_match_values(merged_df, player1, match1_name, col)
+    player2_values = get_player_match_values(merged_df, player2, match2_name, col)
+    
+    # Get team names
+    team1_name = merged_df[merged_df['player'] == player1]['team'].values[0]
+    team2_name = merged_df[merged_df['player'] == player2]['team'].values[0]
+    
+    # Define lower and upper limits for each parameter
+    predefined_low = [0] * len(selected_params)
+    predefined_high = [1] * len(selected_params)
+    
+    low = np.minimum(predefined_low, np.minimum(player1_values, player2_values))
+    high = np.maximum(predefined_high, np.maximum(player1_values, player2_values))
+    
+    # Ensure that high is greater than low for all parameters
+    for i in range(len(low)):
+        if low[i] >= high[i]:
+            high[i] = low[i] + 1  # Adjust high to be greater than low
+    
+    create_radar_chart(selected_params, low, high, lower_is_better, player1_values, player2_values, player1, player2, team1_name, team2_name, match1_name, match2_name)
 
 def get_player_match_values(merged_df, player_name, match_name, col):
     """Get player values for radar chart."""
@@ -25,7 +83,7 @@ def get_player_match_values(merged_df, player_name, match_name, col):
 
 def match_compare():
     with open('league_dict.json', 'r') as f:
-            league_dict = json.load(f)
+        league_dict = json.load(f)
 
     # Create a mapping from league names to keys
     league_name_to_key = {v['FBref']: k for k, v in league_dict.items()}
@@ -80,9 +138,11 @@ def match_compare():
     games = merged_df['game'].unique().tolist()
 
     # Streamlit UI elements
-    selected_game = st.selectbox("Select the match", games)
     player1 = st.selectbox("Select the first player", players)
     player2 = st.selectbox("Select the second player", players)
+
+    selected_game1 = st.selectbox("Select the match for the first player", games)
+    selected_game2 = st.selectbox("Select the match for the second player", games)
 
     params = list(param_mapping.keys())
     selected_params = st.multiselect("Select parameters to compare", params, default=params[:5])
@@ -90,5 +150,4 @@ def match_compare():
     lower_is_better_options = st.multiselect("Select parameters where lower is better", params)
 
     if st.button("Compare Players"):
-        match_df = filter_match_data(merged_df, selected_game)
-        compare_players_and_create_radar(match_df, player1, player2, selected_params, param_mapping, lower_is_better_options)
+        compare_players_and_create_radar(merged_df, player1, player2, selected_game1, selected_game2, selected_params, param_mapping, lower_is_better_options)
