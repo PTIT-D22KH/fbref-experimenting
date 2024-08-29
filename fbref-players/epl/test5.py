0def980b5539a60ecc8b52d151d71185d98b16eb
duongvu5 <vucongtuanduong5@gmail.com>
2024-08-29 02:27:12 +0000

    add files

--- fbref-players/epl/test5.py
+++ fbref-players/epl/test5.py
@@ -30,22 +30,20 @@ def get_stats_lists():
 
 def read_and_filter_stats(fbref, stats_list):
     """Read and filter player season stats."""
-    df_list1 = []
-    df_list2 = []
-    df1_normal, df1_combined = filter_duplicate_players(fbref.read_player_season_stats('standard'))
-    df_list1.append(df1_normal)
-    df_list2.append(df1_combined)
+    df_list = []
+    df1 = fbref.read_player_season_stats('standard')
+    df1_filtered = filter_duplicate_players(df1)
+    df_list.append(df1_filtered)
     dropped_columns = ['nation', 'pos', 'team', 'age', 'born', 'league', 'season']
     
     for stat in stats_list[1:]:
         df = fbref.read_player_season_stats(stat)
         df.drop(columns=dropped_columns, inplace=True)
         df.fillna(0, inplace=True)
-        df_normal, df_combined = filter_duplicate_players(df)
-        df_list1.append(df_normal)
-        df_list2.append(df_combined)
+        df_filtered = filter_duplicate_players(df)
+        df_list.append(df_filtered)
     
-    return df_list1, df_list2
+    return df_list
 
 def filter_duplicate_players(df):
     """Filter out players with 2 or more occurrences."""
@@ -54,7 +52,6 @@ def filter_duplicate_players(df):
 
     # Filter the DataFrame to include only players with multiple occurrences
     df_filtered = df[df['player'].isin(players_to_drop)]
-    df_normal = df[~df['player'].isin(players_to_drop)]
 
     # Separate the columns that should not be summed, excluding 'player' and 'id'
     non_sum_columns = df_filtered.select_dtypes(include='object').columns.tolist()
@@ -94,39 +91,24 @@ def filter_duplicate_players(df):
 
     # Drop duplicates
     group_combined = group_combined.drop_duplicates(keep='first')
-    group_combined = group_combined.set_index('id')
-    # print(group_combined)
-    # combined_df = pd.concat([df_normal, group_combined]).drop_duplicates(keep='last')
-    return df_normal, group_combined
 
-def merge_dataframes(df_list1, df_list2):
-    """Merge two lists of dataframes on the 'player' column."""
-    for i in range(len(df_list1)):
-        df_list1[i] = df_list1[i].reset_index()
-        df_list2[i] = df_list2[i].reset_index()
-    
-    merged_df1 = df_list1[0]
-    merged_df2 = df_list2[0]
-    
-    for i, (df1, df2) in enumerate(zip(df_list1[1:], df_list2[1:]), start=1):
-        merged_df1 = pd.merge(merged_df1, df1, on='player', how='inner', suffixes=('', f'_df{i}'))
-        merged_df2 = pd.merge(merged_df2, df2, on='player', how='inner', suffixes=('', f'_df{i}'))
-    
-    merged_df1.set_index('id', inplace=True)
-    merged_df2.set_index('id', inplace=True)
-    
-    return merged_df1, merged_df2
+    return group_combined
 
-def get_player_values(merged_df1, merged_df2, player_name, col):
-    """Get player values for radar chart from two merged DataFrames."""
-    player_df1 = merged_df1[merged_df1['player'] == player_name]
-    player_df2 = merged_df2[merged_df2['player'] == player_name]
+def merge_dataframes(df_list):
+    """Merge list of dataframes on the 'player' column."""
+    for i in range(len(df_list)):
+        df_list[i] = df_list[i].reset_index()
     
-    if not player_df1.empty:
-        player_df = player_df1
-    else:
-        player_df = player_df2
+    merged_df = df_list[0]
+    for i, df in enumerate(df_list[1:], start=1):
+        merged_df = pd.merge(merged_df, df, on='player', how='inner', suffixes=('', f'_df{i}'))
     
+    merged_df.set_index('id', inplace=True)
+    return merged_df
+
+def get_player_values(merged_df, player_name, col):
+    """Get player values for radar chart."""
+    player_df = merged_df[merged_df['player'] == player_name]
     player_values = np.array(player_df[col[0][0]][col[0][1]].values[0])
     
     for x in col[1:]:
@@ -155,39 +137,33 @@ def create_radar_chart(params, low, high, lower_is_better, player1_values, playe
     fig, axs = mp.grid(figheight=14, grid_height=0.915, title_height=0.06, endnote_height=0.025, title_space=0, endnote_space=0, grid_key='radar', axis=False)
     
     radar.setup_axis(ax=axs['radar'])
-    rings_inner = radar.draw_circles(ax=axs['radar'], facecolor='#ffb2b2', edgecolor='#fc5f5f')
-    radar_output = radar.draw_radar_compare(player1_values, player2_values, ax=axs['radar'], kwargs_radar={'facecolor': '#00f2c1', 'alpha': 0.6}, kwargs_compare={'facecolor': '#d80499', 'alpha': 0.6})
+    rings_inner = radar.draw_circles(ax=axs['radar'], facecolor='#f77b83', edgecolor='#fe2837')
+    radar_output = radar.draw_radar_compare(player1_values, player2_values, ax=axs['radar'], kwargs_radar={'facecolor': '#f9c728', 'hatch': '.', 'alpha': 1}, kwargs_compare={'facecolor': '#e6dedc', 'edgecolor': '#1a1414', 'hatch': '/', 'alpha': 1})
     radar_poly, radar_poly2, vertices1, vertices2 = radar_output
-    range_labels = radar.draw_range_labels(ax=axs['radar'], fontsize=25, fontproperties=robotto_thin.prop)
-    param_labels = radar.draw_param_labels(ax=axs['radar'], fontsize=25, fontproperties=robotto_thin.prop)
+    radar_output2 = radar.draw_radar_compare(player1_values, player2_values, ax=axs['radar'], kwargs_radar={'facecolor': 'None', 'edgecolor': '#646366'}, kwargs_compare={'facecolor': 'None'})
+    range_labels = radar.draw_range_labels(ax=axs['radar'], fontsize=25, fontproperties=serif_extra_light.prop)
+    param_labels = radar.draw_param_labels(ax=axs['radar'], fontsize=25, fontproperties=serif_regular.prop)
     axs['radar'].scatter(vertices1[:, 0], vertices1[:, 1], c='#00f2c1', edgecolors='#6d6c6d', marker='o', s=150, zorder=2)
     axs['radar'].scatter(vertices2[:, 0], vertices2[:, 1], c='#d80499', edgecolors='#6d6c6d', marker='o', s=150, zorder=2)
     
-    endnote_text = axs['endnote'].text(0.99, 0.5, 'Inspired By: StatsBomb / Rami Moghadam', fontsize=15, fontproperties=robotto_thin.prop, ha='right', va='center')
-    title1_text = axs['title'].text(0.01, 0.65, player1_name, fontsize=25, color='#01c49d', fontproperties=robotto_bold.prop, ha='left', va='center')
+    endnote_text = axs['endnote'].text(0.99, 0.5, 'Inspired By: StatsBomb / Rami Moghadam', fontsize=15, fontproperties=serif_extra_light.prop, ha='right', va='center')
+    title1_text = axs['title'].text(0.01, 0.65, player1_name, fontsize=25, color='#01c49d', fontproperties=rubik_regular.prop, ha='left', va='center')
     title2_text = axs['title'].text(0.01, 0.25, team1_name, fontsize=20, fontproperties=robotto_thin.prop, ha='left', va='center', color='#01c49d')
-    title3_text = axs['title'].text(0.99, 0.65, player2_name, fontsize=25, fontproperties=robotto_bold.prop, ha='right', va='center', color='#d80499')
+    title3_text = axs['title'].text(0.99, 0.65, player2_name, fontsize=25, fontproperties=rubik_regular.prop, ha='right', va='center', color='#d80499')
     title4_text = axs['title'].text(0.99, 0.25, team2_name, fontsize=20, fontproperties=robotto_thin.prop, ha='right', va='center', color='#d80499')
     
     fig.set_facecolor('#f2dad2')
     st.pyplot(fig)
 
-def compare_players_and_create_radar(merged_df1, merged_df2, player1, player2, selected_params, param_mapping, lower_is_better):
+def compare_players_and_create_radar(merged_df, player1, player2, selected_params, param_mapping, lower_is_better):
     """Compare two players and create a radar chart."""
     col = [param_mapping[param] for param in selected_params]
-    player1_values = get_player_values(merged_df1, merged_df2, player1, col)
-    player2_values = get_player_values(merged_df1, merged_df2, player2, col)
+    player1_values = get_player_values(merged_df, player1, col)
+    player2_values = get_player_values(merged_df, player2, col)
     
     # Get team names
-    if player1 in merged_df1['player'].values:
-        team1_name = merged_df1[merged_df1['player'] == player1]['team'].values[0]
-    else:
-        team1_name = merged_df2[merged_df2['player'] == player1]['team'].values[0]
-    
-    if player2 in merged_df1['player'].values:
-        team2_name = merged_df1[merged_df1['player'] == player2]['team'].values[0]
-    else:
-        team2_name = merged_df2[merged_df2['player'] == player2]['team'].values[0]
+    team1_name = merged_df[merged_df['player'] == player1]['team'].values[0]
+    team2_name = merged_df[merged_df['player'] == player2]['team'].values[0]
     
     # Define lower and upper limits for each parameter
     predefined_low = [0] * len(selected_params)
@@ -237,7 +213,7 @@ def main():
 
     if comparison_type == 'Normal Footballer':
         stats_list, _, _ = get_stats_lists()
-        df_list1, df_list2 = read_and_filter_stats(fbref, stats_list)
+        df_list = read_and_filter_stats(fbref, stats_list)
         param_mapping = {
             "Goals": ['Performance', 'Gls'],
             "Assists": ['Performance', 'Ast'],
@@ -490,11 +466,9 @@ def main():
             "Aerial Duels Lost": ['Aerial Duels', 'Lost'],
             "Aerial Duels Win Percentage": ['Aerial Duels', 'Won%']
         }
-    merged_df1, merged_df2 = merge_dataframes(df_list1, df_list2)
+    merged_df = merge_dataframes(df_list)
     # Get list of players
-    players = merged_df1['player'].unique().tolist()
-    players.extend(merged_df2['player'].unique().tolist())
-    players = list(set(players))  # Remove duplicates
+    players = merged_df['player'].unique().tolist()
     
     player1 = st.selectbox("Select the first player", players)
     player2 = st.selectbox("Select the second player", players)
@@ -505,7 +479,7 @@ def main():
     lower_is_better_options = st.multiselect("Select parameters where lower is better", params)
     
     if st.button("Compare Players"):
-        compare_players_and_create_radar(merged_df1, merged_df2, player1, player2, selected_params, param_mapping, lower_is_better_options)
+        compare_players_and_create_radar(merged_df, player1, player2, selected_params, param_mapping, lower_is_better_options)
 
 if __name__ == "__main__":
     main()
